23ac3d865bb2b105d5942cf7c864944b9e46dd2a
tuytuy3 <89216147+tuytuy3@users.noreply.github.com>
2021-12-17 22:32:52 +0300

    Add files via upload

---
+++ todos/todos.component.js
@@ -0,0 +1,146 @@
+class TodosComponent {
+    constructor (parent, selector) {
+        this.host = parent.querySelector(selector)
+        this.todosTemplate = new TodosTemplate(this.host)
+
+        this.todos = [
+            {
+                title: 'lorsad foasifasjfasoi hfsai;saasdlias oashfasoihfaslfi hasoifhasogfha sohasoihasoi',
+                id: 1,
+                done: false
+            },
+            {
+                title: 'lorsa dfoasifa sjfasoih fsai;sa',
+                id: 2,
+                done: false
+            },
+        ];
+
+        this.chosenTodos = '';
+
+        this.language = {
+            en: true,
+            ua: false
+        };
+
+        this.state = {
+            uiMode: 'light',
+            uiModeTypes: ['light', 'dark'],
+        };
+        
+        this.renderAll()
+        this.todosTemplate.onAddTodo(this.addTodo.bind(this));
+        this.todosTemplate.onToggleDone(this.toggleDone.bind(this));
+        this.todosTemplate.onTodosBtns(this.chooseTodosBtn.bind(this));
+        this.todosTemplate.onDeleteTodo(this.deleteTodo.bind(this));
+        this.todosTemplate.onChangeLang(this.changeLang.bind(this));
+        this.todosTemplate.onChangeTheme(this.changeUiMode.bind(this))
+    }
+    
+    changeUiMode() {
+        this.state = {...this.state, uiMode: this.state.uiModeTypes.filter(uiMode => this.state.uiMode !==uiMode)[0]};
+        this.todosTemplate.renderTheme(this.state.uiMode)
+    }
+
+    renderLang() {
+        if (this.language.en) {
+            this.todosTemplate.renderUI('en', this.todosCounter())
+        } else if(this.language.ua) {
+            this.todosTemplate.renderUI('ua', this.todosCounter())
+        }
+    }
+
+    renderAll(todos = this.todos) {
+        if (this.chosenTodos === 'done') {
+            todos = this.showDoneTodos()
+        } else if (this.chosenTodos === 'false') {
+            todos = this.showNotDoneTodos()
+        } else if (this.chosenTodos === 'all') {
+            todos = this.showAll()
+        }
+        this.todosTemplate.renderTodos(todos)
+        // this.todosTemplate.renderTodosCounter(this.todosCounter())
+        this.renderLang();
+    }
+
+    addTodo(title) {
+        if(!title) {
+            return
+        }
+
+        const newTodo = {
+            title,
+            id: this.getNewTodoId(),
+            done: false
+        }
+
+        this.todos = [...this.todos, newTodo];
+
+        this.renderAll()
+    }
+
+    getNewTodoId() {
+        return this.todos.length ? this.todos[this.todos.length - 1].id + 1 : 1
+    }
+
+    todosCounter() {
+        return this.todos.filter(todo => todo.done === false).length
+    }
+
+    toggleDone(id) {
+        this.todos = this.todos.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo);
+        this.renderAll()
+    }
+
+    chooseTodosBtn(btnClass) {
+        let todos = []
+        if (btnClass === 'todos__del--btn') {
+            todos = this.deleteDoneTodos()
+        } else if (btnClass === 'todos__show1--btn') {
+            todos = this.showDoneTodos()
+        } else if (btnClass === 'todos__show2--btn') {
+            todos = this.showNotDoneTodos()
+        } else if (btnClass === 'todos__show3--btn') {
+            todos = this.showAll()
+        }
+
+        this.renderAll(todos)
+    }
+
+    deleteDoneTodos() {
+        this.todos = this.todos.filter(todo => todo.done === false);
+        return this.todos
+    }
+
+    showDoneTodos() {
+        let doneTodos = this.todos.filter(todo => todo.done === true);
+        this.chosenTodos = 'done';
+        return doneTodos
+    }
+
+    showNotDoneTodos() {
+        let notDoneTodos = this.todos.filter(todo => todo.done === false);
+        this.chosenTodos = 'false';
+        return notDoneTodos
+    }
+
+    showAll() {
+        this.chosenTodos = 'all';
+        return this.todos
+    }
+
+    deleteTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id)
+        this.renderAll()
+    }
+
+    changeLang(btnClass) {
+        if (btnClass === 'todos__en--lang') {
+            this.language = {...this.language, en: true, ua: false}
+        } else if (btnClass === 'todos__ua--lang') {
+            this.language = {...this.language, en: false, ua: true}
+        }
+        this.renderLang()
+    }
+
+} 
---
+++ todos/todos.template.js
@@ -0,0 +1,190 @@
+class TodosTemplate {
+    constructor(host) {
+        this.host = host
+
+        this.iniTemplate = `
+        <div class="todos">
+                <div class="todos__header">
+                    <div class="todos__header__top">
+                        <div class="todos__theme"><img src="https://www.svgrepo.com/show/34399/light-bulb.svg" class="svg"></div>
+                        <h1 class="todos__title">To-do App</h1>
+                        <div class="todos__en--lang"><img src="https://flagicons.lipis.dev/flags/4x3/gb.svg" class="svg"></div>
+                        <div class="todos__ua--lang"><img src="https://flagicons.lipis.dev/flags/4x3/ua.svg" class="svg"></div>
+                    </div>
+                    <form class="todos__form">
+                        <input type="text" class="todos__form__field" placeholder="Add To-do Here"> <button class="todos__form--btn">+</button>
+                    </form>
+                    <div class="todos__info">
+                        <button class="todos__del--btn">Remove done</button>
+                        <button class="todos__show1--btn">Show done</button>
+                        <button class="todos__show2--btn">Show not done</button>
+                        <button class="todos__show3--btn">Show All</button>
+                    </div>
+                </div>
+                <p class="todos__counter">You have 5 todos left</p>
+                <ul class="todos__list">
+                </ul>
+            </div>
+        `;
+
+        this.initRender();
+
+        this.todosHeader = this.host.querySelector('.todos__header__top')
+        this.form = this.host.querySelector('.todos__form');
+        this.field = this.form.querySelector('.todos__form__field');
+        this.formBtn = this.form.querySelector('.todos__form--btn')
+        this.todosInfo = this.host.querySelector('.todos__info');
+        this.todosDelBtn = this.todosInfo.querySelector('.todos__del--btn');
+        this.todosShowBtn1 = this.todosInfo.querySelector('.todos__show1--btn');
+        this.todosShowBtn2 = this.todosInfo.querySelector('.todos__show2--btn');
+        this.todosShowBtn3 = this.todosInfo.querySelector('.todos__show3--btn');
+        this.list = this.host.querySelector('.todos__list');
+        this.todosCounter = this.host.querySelector('.todos__counter');
+        this.uaLangBtn = this.host.querySelector('.todos__ua--lang');
+        this.enLangBtn = this.host.querySelector('.todos__en--lang');
+        this.todosTheme = this.host.querySelector('.todos__theme')
+
+    }
+
+    initRender() {
+        this.host.innerHTML = this.iniTemplate;
+    }
+
+    renderTheme (uiMode) {
+        if (uiMode === 'light') {
+            this.todosTheme.innerHTML = `<img src="https://www.svgrepo.com/show/34399/light-bulb.svg" class="svg">`;
+            this.turnLight();
+        } else if (uiMode === 'dark') {
+            this.todosTheme.innerHTML = `<img src="https://www.svgrepo.com/show/17950/red-light.svg" class="svg">`;
+            this.turnDark();
+        }
+    }
+
+    turnLight() {
+        this.host.parentElement.classList.toggle('app__dark');
+        this.todosHeader.classList.toggle('todos__header__top__dark');
+        this.field.classList.toggle('todos__form__field__dark');
+        this.formBtn.classList.toggle('todos__form--btn__dark');
+        console.log(this.formBtn);
+    }
+
+    turnDark () {
+        this.host.parentElement.classList.toggle('app__dark');
+        this.todosHeader.classList.toggle('todos__header__top__dark');
+        this.field.classList.toggle('todos__form__field__dark');
+        this.formBtn.classList.toggle('todos__form--btn__dark');
+    }
+
+    renderUI(lang, length) {
+        const todosTitle = this.host.querySelector('.todos__title');
+        this.field;
+
+        this.todosCounter;
+
+        if (lang === 'en') {
+            todosTitle.innerHTML = 'To-do App';
+            this.field['placeholder'] = 'Add To-do Here';
+            this.todosDelBtn.innerHTML = 'Remove done';
+            this.todosShowBtn1.innerHTML = 'Show done';
+            this.todosShowBtn2.innerHTML = 'Show not done';
+            this.todosShowBtn3.innerHTML = 'Show All';
+            this.renderTodosCounter(length, lang)
+
+        } else if (lang === 'ua') {
+            todosTitle.innerHTML = 'Список справ';
+            this.field['placeholder'] = 'Додайте справу сюди';
+            this.todosDelBtn.innerHTML = 'Видалити виконані';
+            this.todosShowBtn1.innerHTML = 'Показати виконані';
+            this.todosShowBtn2.innerHTML = 'Показати невиконані';
+            this.todosShowBtn3.innerHTML = 'Показати всі';
+            this.renderTodosCounter(length, lang)
+        }
+    }
+
+    renderTodos(todos) {
+        this.list.innerHTML = ``;
+        todos.forEach(todo => {
+            this.list.innerHTML += `
+            <li class="todos__todo">
+                <div class="flex">
+                    <input type="checkbox" class="todos__checkbox" ${todo.done ? 'checked' : ''} data-id="${todo.id}">
+                </div>
+                <span class="todos__todo__text">
+                    ${todo.title}
+                </span> 
+                <div class="flex">
+                    <button class="todo__del--btn">DEL</button>
+                </div>
+            </li>
+            <li class="todos__line"></li>
+            `
+        })
+        this.cleanField()
+    }
+
+    cleanField() {
+        this.field.value = null
+    }
+
+    renderTodosCounter(counter, lang = 'en') {
+        if (lang === 'en') {
+            this.todosCounter.innerHTML = counter === 1 ? `You have ${counter} todo left` : `You have ${counter} todos left`
+        } else if (lang === 'ua') {
+            this.todosCounter.innerHTML = counter === 1 || counter === 21 || counter === 31 ? `У вас залишилась ${counter} справа` : `У вас залишилось ${counter} справ(и)`
+        }
+    }
+
+    onAddTodo(cb) {
+        this.form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            const title = this.field.value.trim()
+
+            cb(title)
+        })
+    }
+
+    onToggleDone(cb) {
+        this.list.addEventListener('change', (e) => {
+            const id = +e.target.getAttribute('data-id')
+            cb(id)
+        })
+    }
+
+    onTodosBtns(cb) {
+        this.todosInfo.addEventListener('click', (e) => {
+            const btnClass = e.target.getAttribute('class');
+            console.log(btnClass);
+            cb(btnClass)
+        })
+    }
+
+    onDeleteTodo(cb) {
+        this.list.addEventListener('click', (e) => {
+            if (e.target.className === 'todo__del--btn') {
+                let id = +e.target.parentNode.parentElement.querySelector('.todos__checkbox').getAttribute('data-id')
+                cb(id)
+            }
+        })
+    }
+
+    onChangeLang(cb) {
+        this.uaLangBtn.addEventListener('click', (e) => {
+            const btnClass = e.target.closest('div').getAttribute('class');
+            cb(btnClass)
+        });
+
+        this.enLangBtn.addEventListener('click', (e) => {
+            const btnClass = e.target.closest('div').getAttribute('class');
+            cb(btnClass)
+        })
+    } 
+
+    onChangeTheme(cb) {
+        this.todosTheme.addEventListener('click', (e) => {
+            if (e.target.closest('div').getAttribute('class') === 'todos__theme') {
+                cb()
+            }
+        })
+    }
+
+}
